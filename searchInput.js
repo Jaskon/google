01f4dac2f9a472868761677ebb9063c16106b659
Jaskon <jaskon97@gmail.com>
2016-07-13 12:32:55 +0300

    google

---
+++ searchInput.js
@@ -0,0 +1,6 @@
+function searchInputFocused(elem) {
+	elem.parentNode.style.borderColor = "#4d90fe";
+}
+function searchInputUnfocused(elem) {
+	elem.parentNode.style.borderColor = "";
+}
---
+++ subNavBar.js
@@ -0,0 +1,22 @@
+function subNavBar() {
+	this.offset = 5;
+	this.accept = function(object, parent) {
+		object.classList.add("subNavBar");
+		var left = parent.offsetLeft + (parent.clientWidth - object.clientWidth)/2;
+		if (left < 5)
+			left = 5;
+		else if (left > document.body.clientWidth - object.clientWidth - this.offset)
+			left = document.body.clientWidth - object.clientWidth - this.offset;
+		object.style.left = left;
+	}
+
+	this.recount = function(object, parent) {
+		object.style.top = parent.offsetTop + parent.clientHeight + this.offset;
+		var left = parent.offsetLeft + (parent.clientWidth - object.clientWidth)/2;
+		if (left < 5)
+			left = 5;
+		else if (left > document.body.clientWidth - object.clientWidth - this.offset)
+			left = document.body.clientWidth - object.clientWidth - this.offset;
+		object.style.left = left;
+	}
+}
